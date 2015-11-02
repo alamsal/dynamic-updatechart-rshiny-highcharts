75cd116ad662cbfc3d6724a3e475302f1a5c6067
unknown <aashislamsal@AFSPCHPA23810BJ.usda.net>
2015-11-02 15:47:44 -0700

    Working prototype all in one

---
+++ server.R
@@ -0,0 +1,19 @@
+# server.R
+ 
+shinyServer(function(input, output, session) {
+ 
+	output$results = renderPrint({
+        input$mydata
+    })
+	
+	# observes if value of mydata sent from the client changes.  if yes
+	# generate a new random color string and send it back to the client
+	# handler function called 'myCallbackHandler'
+	observe({
+		input$mydata
+		color = rgb(runif(1), runif(1), runif(1))
+		
+		session$sendCustomMessage(type = "myCallbackHandler", color)
+	})
+ 
+})
---
+++ ui.R
@@ -0,0 +1,37 @@
+# ui.R
+ 
+shinyUI( bootstrapPage(
+ 
+  # a div named mydiv
+  tags$div(id="mydiv", style="width: 50px; height :50px;
+           left: 100px; top: 100px;
+           background-color: red; position: absolute"),
+ 
+  # a shiny element to display unformatted text
+  verbatimTextOutput("results"),
+  
+  # javascript code to send data to shiny server
+  tags$script('
+    document.getElementById("mydiv").onclick = function() {
+      var number = Math.random();	   
+      Shiny.onInputChange("mydata", number);
+    };
+  '),
+
+  
+  # handler to receive data from server
+	tags$script('
+	  Shiny.addCustomMessageHandler("myCallbackHandler",
+			function(color) {
+			  document.getElementById("mydiv").style.backgroundColor = color;
+			});
+	'),
+	
+   # handler to receive custom data
+   includeScript("https://code.highcharts.com/modules/exporting.js"),
+   includeScript("https://code.highcharts.com/highcharts.js"),
+   tags$div(id ="container", style ="min-width: 310px; height: 400px; margin: 0 auto;position: absolute"),
+   includeScript("charts.js")
+   
+ 
+))
